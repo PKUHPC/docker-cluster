2a6b980d58bba6d27c6e327969fdec951d39a4d6
huangjun0210 <huangjun0210-17@126.com>
2023-07-21 15:08:51 +0800

    优化docker-entrypoint.sh

--- bulid/docker-entrypoint.sh
+++ bulid/docker-entrypoint.sh
@@ -17,23 +17,15 @@ then
         done
     }
     echo "-- Database is now active ..."
-    #systemctl restart sssd
-   # systemctl start systemd-logind
-   # systemctl start oddjobd
     supervisord -c /etc/supervisor/conf.d/slurmdbd_supervisord.conf
-    #exec gosu slurm /usr/sbin/slurmdbd -Dvvv
+
 fi
 
 if [ "$1" = "slurmctld" ]
 then
     echo "---> Starting the MUNGE Authentication service (munged) ..."
-    #supervisord -c /etc/supervisor/conf.d/supervisord.conf
-    #nohup /adapter/scow-slurm-adapter-amd64 > /adapter/server.log 2>&1 &
     gosu munge /usr/sbin/munged
     
-    #systemctl restart sssd
-    #systemctl start systemd-logind
-    #systemctl start oddjobd
     echo "---> Waiting for slurmdbd to become active before starting slurmctld ..."
 
     until 2>/dev/null >/dev/tcp/slurmdbd/6819
@@ -47,7 +39,6 @@ then
     if /usr/sbin/slurmctld -V | grep -q '17.02' ; then
         exec gosu slurm /usr/sbin/slurmctld -Dvvv
     else
-        #exec gosu slurm /usr/sbin/slurmctld -i -Dvvv
         supervisord -c /etc/supervisor/conf.d/slurmctld_supervisord.conf
     fi
 fi
@@ -55,12 +46,8 @@ fi
 if [ "$1" = "slurmd" ]
 then
     echo "---> Starting the MUNGE Authentication service (munged) ..."
-    #supervisord -c /etc/supervisor/conf.d/supervisord.conf
     gosu munge /usr/sbin/munged
-    #systemctl restart sssd
-    #systemctl start systemd-logind
-    #systemctl start oddjobd    
-
+       
     echo "---> Waiting for slurmctld to become active before starting slurmd..."
 
     until 2>/dev/null >/dev/tcp/slurmctld/6817
@@ -71,7 +58,6 @@ then
     echo "-- slurmctld is now active ..."
 
     echo "---> Starting the Slurm Node Daemon (slurmd) ..."
-   # exec /usr/sbin/slurmd -Dvvv
     supervisord -c /etc/supervisor/conf.d/slurmd_supervisord.conf
 fi
 
